669892ae13508117a59511190be33b4c4150208d
rafai123 <rafaimhd123@gmail.com>
2023-01-24 11:43:34 +0700

    Melakukan copy file dari Latihan 12 dan menambahkan fitur upload foto

---
+++ Latihan13/function.php
@@ -0,0 +1,141 @@
+<?php
+    // Buat Koneksi
+    $conn = Mysqli_connect("localhost", "root", "", "mahasiswa");
+
+    function query($query) {
+        global $conn;
+        $rows = [];
+
+        $result = Mysqli_query($conn, $query);
+
+        while ($row = Mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+        
+    return $rows;
+    }
+
+
+    function tambah($data) {
+        global $conn;
+        // tangkap isi data
+        $nim = $data["nim"];
+        $nama = $data["nama"];
+        $jurusan = $data["jurusan"];
+        $email = $data["email"];
+
+        $gambar = upload();
+
+        // jika function gambar menghasilkan false, maka gagal tambah data
+        if ($gambar == false) {
+            return false;
+        }
+
+          // query
+        Mysqli_query($conn,"INSERT INTO biodata
+                            VALUES
+                            ('', '$nama', '$nim', '$jurusan', '$email', '$gambar') 
+                            " );
+
+    return Mysqli_affected_rows($conn);
+    }
+
+    function delete($id) {
+        global $conn;
+
+        Mysqli_query($conn, "DELETE FROM biodata WHERE id = $id");
+
+    return Mysqli_affected_rows($conn);
+    }
+
+    function update($data) {
+        global $conn;
+
+        $id = $data["id"];
+        $name =$data["nama"];
+        $nim = $data["nim"];
+        $major = $data["jurusan"];
+        $email = $data["email"];
+
+        if ( $_FILES["gambar"]["error"] === 4) {
+            $img = $data["gambar"];
+        } else {
+            $img = upload();
+        }
+
+        Mysqli_query($conn, "UPDATE biodata
+                            SET 
+                            
+                            nama = '$name',
+                            nim = '$nim',
+                            jurusan = '$major',
+                            email = '$email',
+                            gambar = '$img'
+                            WHERE id = $id
+                            ");
+
+    return Mysqli_affected_rows($conn);
+    }
+
+    function search($keyword) {
+        global $conn;
+
+        $rows = [];
+
+        $result = Mysqli_query($conn, "SELECT * FROM biodata
+                                WHERE
+                                nama LIKE '%$keyword%' OR
+                                nim LIKE '%$keyword%' OR
+                                jurusan LIKE '%$keyword' OR
+                                email LIKE '%$keyword%'
+                                ");
+        while ( $row = Mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+
+    function upload() {
+        $name = $_FILES["gambar"]["name"];
+        $tmp_name = $_FILES["gambar"]["tmp_name"];
+        $error = $_FILES["gambar"]["error"];
+        $size = $_FILES["gambar"]["size"];
+
+        // cek apakah file sudah di masukkan
+        if ( $error === 4 ) {
+            echo "
+                <script>
+                    alert('Silahkan Masukkan Gambar');
+                </script>
+            ";
+            return false;
+        }
+
+        // cek apakah extensi file adalah gambar
+        $valid_extension = ["jpg", "jpeg", "png", "gif"];
+        $explode_name = explode(".", $name);
+        $image_extension = strtolower(end($explode_name));
+
+        if (!in_array($image_extension,$valid_extension)) {
+            echo "
+                <script>
+                    alert('File yang anda upload bukan gambar!');
+                </script>
+            ";
+            return false;
+        }
+        // var_dump($image_extension); die;
+
+        // buat nama file menjadi id unik
+        $new_file_name = uniqid();
+        $new_file_name .= "_";
+        $new_file_name .= $explode_name[0];
+        $new_file_name .= ".";
+        $new_file_name .= $image_extension;
+        // jika file sudah memenuhi syarat, maka lakukan upload
+        move_uploaded_file($tmp_name, "img/" . $new_file_name);
+
+        return $new_file_name;
+        
+    }
+?>
---
+++ Latihan13/tambah.php
@@ -0,0 +1,84 @@
+<?php
+    require "function.php";
+
+    if ( isset($_POST["submit"]) ) {
+        if ( tambah($_POST) > 0 ) {
+            echo "
+                <script>
+                    alert('tambah data berhasil');
+                    document.location.href = 'index.php';
+                </script>
+            ";
+        } else {
+            echo "
+                <script>
+                    alert('tambah data gagal');
+                    
+                </script>
+            ";
+        }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+</head>
+<body>
+    <h1>Tambah Data</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td>
+                    <label for="nama">Nama</label>
+                </td>
+                <td>
+                    <input type="text" name="nama" id="nama">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="nim">NIM</label>
+                </td>
+                <td>
+                    <input type="text" name="nim" id="nim">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="jurusan">Jurusan</label>
+                </td>
+                <td>
+                    <input type="text" name="jurusan" id="jurusan">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email</label>
+                </td>
+                <td>
+                    <input type="text" name="email" id="email">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="gambar">Gambar</label>
+                </td>
+                <td>
+                    <input type="file" name="gambar" id="gambar">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button type="submit" name="submit">Tambah Data</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
