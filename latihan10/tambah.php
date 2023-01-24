b0d016dcd836874102a5b663a9ada0d263b671b1
rafai123 <rafaimhd123@gmail.com>
2023-01-24 11:34:50 +0700

    Menambahkan file tambah.php untuk form tambah data ke database

---
+++ latihan10/tambah.php
@@ -0,0 +1,68 @@
+<?php
+
+    require "function.php";
+
+    if ( isset($_POST["submit"]) ) {
+        if ( tambah($conn, $_POST) > 0  ) {
+            echo "
+                <script>
+                    alert('Tambah Data Berhasil');
+                    document.location.href = 'index.php';
+                </script>;
+            ";
+        } else {
+            echo "
+            <script>alert('Tambah Data Gagal')</script>";
+            echo Mysqli_error($conn);
+        }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tambah Data</h1>
+
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td><label for="nama">Nama : </label></td>
+                <td><input type="text" name="nama" id="nama"></td>
+            </tr>
+            <tr>
+                <td><label for="nim">NIM : </label></td>
+                <td><input type="text" name="nim" id="nim"></td>
+            </tr>
+            <tr>
+                <td><label for="jurusan">Jurusan : </label></td>
+                <td><input type="text" name="jurusan" id="jurusan"></td>
+            </tr>
+            <tr>
+                <td><label for="email">Email : </label></td>
+                <td><input type="text" name="email" id="email"></td>
+            </tr>
+            <tr>
+                <td><label for="gambar">Gambar : </label></td>
+                <td><input type="text" name="gambar" id="gambar"></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="submit" name="submit">Tambah Data</button></td>
+            </tr>
+        </table>
+    </form>
+
+    <p>
+        <a href="index.php">Kembali</a>
+    </p>
+</body>
+</html>
